05784cfab76fa17540059ca0f509d84fb2bb5f5a
Przemek <przemyslaw.walkowski@bccgroup.com>
2018-05-23 13:11:32 +0200

    Add update script builder

--- SelgrosCopy/Program.cs
+++ SelgrosCopy/Program.cs
@@ -16,10 +16,13 @@ namespace SelgrosCopy
 
             DirectoryInfo dirInfo;
 
+            string updateScript = string.Empty;
+
             switch (args[0])
             {
                 case "RO":
                     dirInfo = Directory.CreateDirectory(Path.Combine(@"D:\OneDrive - Business Consulting Center Sp. z o.o\Komputer Rosja-Rumunia\Rumunia\", DateTime.Now.ToString("dd.MM.yyyy")));
+                    updateScript = new UpdateScriptBuilder().Build(file.Name, args[1]);
                     break;
                 default:
                     dirInfo = Directory.CreateDirectory(Path.Combine(@"D:\OneDrive - Business Consulting Center Sp. z o.o\Komputer Rosja-Rumunia\Rosja\", DateTime.Now.ToString("dd.MM.yyyy")));
@@ -27,19 +30,21 @@ namespace SelgrosCopy
             }
 
 
-            File.Copy(@"D:\Projects\Selgros\PG\trunk\DBScripts\SelgrosPG_Schema2.sql", Path.Combine(dirInfo.FullName, "SelgrosPG_Schema2.sql") );
+            File.Copy(@"D:\Projects\Selgros\PG\trunk\DBScripts\SelgrosPG_Schema2.sql", Path.Combine(dirInfo.FullName, "SelgrosPG_Schema2.sql"),true );
 
             Console.WriteLine($"Copy file SelgrosPG_Schema2.sql to {dirInfo.FullName} successful");
 
-            File.Copy(@"D:\Projects\Selgros\PG\trunk\DBScripts\SelgrosPG_Translations.sql", Path.Combine(dirInfo.FullName, "SelgrosPG_Translations.sql") );
+            File.Copy(@"D:\Projects\Selgros\PG\trunk\DBScripts\SelgrosPG_Translations.sql", Path.Combine(dirInfo.FullName, "SelgrosPG_Translations.sql"),true );
             
             Console.WriteLine($"Copy file SelgrosPG_Translations.sql to {dirInfo.FullName} successful");
 
-            File.Copy(file.FullName, Path.Combine(dirInfo.FullName, file.Name));
-
+            File.Copy(file.FullName, Path.Combine(dirInfo.FullName, file.Name),true);
 
             Console.WriteLine($"Copy file {file.Name} to {dirInfo.FullName} successful");
 
+            File.WriteAllText(Path.Combine(dirInfo.FullName, "update.bat"),updateScript);
+
+            Console.WriteLine($"Update script generated successful");
         }
     }
 }
---
+++ SelgrosCopy/UpdateScriptBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SelgrosCopy
+{
+    class UpdateScriptBuilder
+    {
+        public UpdateScriptBuilder()
+        {
+
+        }
+
+        public string Build(string fileName, string version)
+        {
+            var sb = new StringBuilder();
+
+            sb.Append(@"d:\Przemek\ApplicationUpdate\ApplicationUpdater.exe ");
+            sb.Append($"c:\\Users\\InteractiveS\\Downloads\\{fileName} ");
+            sb.Append(@"D:\Przemek\Aktualizacje\ ");
+            sb.Append(@"d:\inetpub\SelgrosPG\ ");
+            sb.Append("{version} ");
+            sb.Append("false");
+
+            return sb.ToString();
+        }
+    }
+}
