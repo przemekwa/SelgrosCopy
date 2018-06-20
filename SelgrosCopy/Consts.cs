d06da519f1cbb57ca3666d8af3d2fdd50ab3a468
Przemek <przemyslaw.walkowski@bccgroup.com>
2018-06-20 16:26:41 +0200

    change destination paths

--- SelgrosCopy/Consts.cs
+++ SelgrosCopy/Consts.cs
@@ -8,9 +8,9 @@ namespace SelgrosCopy
     {
         public static string Schema2FilePath = @"D:\Projects\Selgros\PG\trunk\DBScripts\SelgrosPG_Schema2.sql";
         public static string TranslationsFilePath = @"D:\Projects\Selgros\PG\trunk\DBScripts\SelgrosPG_Translations.sql";
-        public static string PolandDestinationPath = @"D:\Selgros-Aktualizacja";
-        public static string RomaniaDestinationPath = @"D:\OneDrive - Business Consulting Center Sp. z o.o\Komputer Rosja-Rumunia\Rumunia\";
-        public static string RussiaDestinationPath = @"D:\OneDrive - Business Consulting Center Sp. z o.o\Komputer Rosja-Rumunia\Rosja\";
+        public static string PolandDestinationPath = @"D:\selgros-app-update";
+        public static string RomaniaDestinationPath = @"D:\OneDrive - SNP Schneider-Neureither & Partner SE\hpProBook\romania\";
+        public static string RussiaDestinationPath = @"D:\OneDrive - SNP Schneider-Neureither & Partner SE\hpProBook\russia\";
 
     }
 }
